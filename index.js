d684e1c5fc191d8ef42b6e3a0dd557746c554202
Lucia <pongu80@gmail.com>
2021-10-28 16:01:06 -0500

    class practice

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const finishedReading = document.querySelector('#finishedReading');
 
-function Book (title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-};
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+
+    
+}
 
 let myLibrary = [];
 const index = 0;
